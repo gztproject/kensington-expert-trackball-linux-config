6d2cffed3e3dfc91895f5b3e30ae2d873d6efe49
GZTproject <gasper.doljak@gmail.com>
2023-03-16 12:07:51 +0100

    Update Kensington_Expert_Setup.sh
    Fixed so it works better for me :)

--- Kensington_Expert_Setup.sh
+++ Kensington_Expert_Setup.sh
@@ -21,15 +21,16 @@ if [[ ! -z "$check" ]]; then
 	mouse_id=$(xinput | grep "$mouse_name" | sed 's/^.*id=\([0-9]*\)[ \t].*$/\1/')
 	# swap right and back button then swap middle and back button
 	xinput set-button-map $mouse_id 1 8 2 4 5 6 7 3 9
-	# enable better scrolling 
-	xinput set-prop $mouse_id "libinput Natural Scrolling Enabled" 1
-	# disable acceliration for the ball
-	xinput set-prop $mouse_id "libinput Accel Profile Enabled" 0, 1
+	# disable better scrolling 
+	xinput set-prop $mouse_id "libinput Natural Scrolling Enabled" 0
+	# enable acceliration for the ball
+	xinput set-prop $mouse_id "libinput Accel Profile Enabled" 1, 0	
 
 	# allow scrolling by holding middle mouse button and using the ball to scroll ( really smooth and fast ). 
 	xinput set-prop $mouse_id "libinput Scroll Method Enabled" 0, 0, 1
 	# allow the remmaped middle mouse to be used for middle mouse scroll
 	xinput set-prop $mouse_id "libinput Button Scrolling Button" 3
+	
 fi
 
 # read more here https://askubuntu.com/questions/492744/how-do-i-automatically-remap-buttons-on-my-mouse-at-startup
